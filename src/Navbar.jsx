30cc13d8c8feb1d2705b14f7c989d0cf847d805c
kitamura0102 <raul_almonte001@hotmail.com>
2024-11-09 02:07:33 -0400

    Just started

---
+++ src/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>Buy</li>
+        <li>Rent</li>
+        <li>Sell</li>
+        <li>Home Loans</li>
+        <li>Find an Agent</li>
+
+        <li>Manage Rentals</li>
+        <li>Advertise</li>
+        <li>Help</li>
+        <li>Sign In</li>
+      </ul>
+      <img src="./assets/z-logo-default.svg "width="70px" alt="Nombre" />
+    </nav>
+  );
+}
