4812dc9d41c2bff3b8b3d41bcb6b8084b827f19e
kitamura0102 <raul_almonte001@hotmail.com>
2024-11-10 22:37:40 -0300

    nothing yet

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,22 @@ import Navbar from './Navbar.jsx'
 import './App.css'
 import React from 'react'
 import Search from './Search.jsx'
+import Header from "./Header.jsx"
+import { propiedades } from './assets/propiedades.js'
 function App() {
-
+const cards = propiedades.map(item =>{
+  return(
+      <Header item={item}/>
+  )
+})
 
   return (
     <>
       <div>
         <Navbar />
         <Search />
+        {cards}
+        
       </div>
       
     </>
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,1 +1,12 @@
 import React from "react";
+
+export default function Header(props){
+    return(
+        <header>
+            <div className="card">
+                <img src={`./assets/${props.item.picture}`} alt="" width="60px"/>
+                <p>{props.item.direccion}</p>
+            </div>
+        </header>
+    )
+}
